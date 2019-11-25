1249de28be231b3aeb73e45257302f8f5019ca28
MegY <meghdadyazdi@yahoo.com>
2019-11-25 17:44:22 +0000

    Up&Down and Side Traversing

--- js/script.js
+++ js/script.js
@@ -2,7 +2,25 @@
 $(document).ready(function() {
 	//sets <a> element within paragraph  to yellow  
 	$("p").click(function(){
-		$(this).children("a").css("background-color", "yellow"); /* returns all the <a> child elements that are 
-		within this paragraph*/
-	});
+		$(this).children("a").css("background-color", "yellow"); 
+    });
+    
+    // $("button").click(function(){
+	// 	$(this).prev().slideToggle('slow');
+    // });
+    
+    // $(".card_image").click(function(){
+	// 	$(this).next().children('p').slideDown('slow');
+    // });
+    
+    $(".card").click(function(){
+		$(this).toggleClass('highlight'); // add a class by first click and remove it by second click
+    });
+
+    $("#select_btn").click(function(){
+		$('.card:not(.highlight)').hide('slow'); // add a class by first click and remove it by second click
+    });
+     $("#all_btn").click(function(){
+		$('.card').show('slow'); // add a class by first click and remove it by second click
+    });
 });
